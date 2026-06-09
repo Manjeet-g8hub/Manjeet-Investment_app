7cc375a65626d4b407e8b6cbd952ef9d4b3b3de8
Manjeet-g8hub <ahalawat1986@gmail.com>
2026-06-09 12:35:44 +0000

    added slider to investment amt

--- Investment-app.py
+++ Investment-app.py
@@ -10,9 +10,11 @@ try:
     st.title("_AI_ is :blue[cool] :sunglasses:")
     # Input fields for height and weight
     name = st.text_input("* Enter Your name", key="name")
-    inv = st.number_input("Enter Your investment amount", key= "inv")
+    inv = st.slider("Enter Your investment amount",  min_value=100000, max_value= 100000000, step=100000)
     age = st.slider("Enter your Age:", min_value=18, max_value= 60, step=1)
-    time = st.slider("Enter your time horizon:", min_value=100000, max_value= 100000000, step=100000)
+    time = st.slider("Enter your time horizon:", min_value=5, max_value= 30, step=1)
+
+    inv2 = st.number_input()
     
 
     # Calculate BMI
