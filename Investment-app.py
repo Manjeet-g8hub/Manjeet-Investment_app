06d96ff2d92831f905e828f201c98e5f876c2a69
Manjeet-g8hub <ahalawat1986@gmail.com>
2026-06-09 10:28:08 +0000

    commit-V1

---
+++ Investment-app.py
@@ -0,0 +1,34 @@
+try:
+    import streamlit as st
+    import google.genai as genai
+    
+    google_api_key = st.secrets["google"]["api_key"]
+    client = genai.Client(api_key = google_api_key)
+    
+    st.title("Your Personalised AI Investment Assistant")
+    
+    st.title("_AI_ is :blue[cool] :sunglasses:")
+    # Input fields for height and weight
+    name = st.text_input("* Enter Your name", key="name")
+    inv = st.number_input("Enter Your investment amount", key= "inv")
+    age = st.slider("Enter your Age:", min_value=18, max_value= 60, step=1)
+    time = st.slider("Enter your time horizon:", min_value=100000, max_value= 100000000, step=100000)
+    
+
+    # Calculate BMI
+    if st.button("Share Stratigy"):
+        if not name.strip():
+            st.error("Please enter your name.")
+        
+        else:
+            # bmi = wt / (ht ** 2)
+            # st.write(f"Your BMI is: {bmi:.2f}")
+            prompt = f"Greet {name} and Act like an investment expert, share the investment stratagy based on the amount {inv}, over {time} horizon depending on the age {age}"
+        
+            # Generate content from Gemini
+            response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents= prompt)
+            st.write(response.text)
+except Exception as e:
+    st.error(f"An error occurred: {e}")
