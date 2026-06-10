df014b1ee9aa45b549b5571d4faf8a67be3ec980
Manjeet-g8hub <ahalawat1986@gmail.com>
2026-06-10 05:18:40 +0000

    push to Github

--- Investment-app.py
+++ Investment-app.py
@@ -8,12 +8,11 @@ try:
     st.title("Your Personalised AI Investment Assistant")
     
     st.title("_AI_ is :blue[cool] :sunglasses:")
+    # Input fields for name, inv amt, age and time horizon
     name = st.text_input("* Enter Your name", key="name")
     inv = st.slider("Enter Your investment amount",  min_value=100000, max_value= 100000000, step=100000)
     age = st.slider("Enter your Age:", min_value=18, max_value= 60, step=1)
     time = st.slider("Enter your time horizon:", min_value=5, max_value= 30, step=1)
-
-    inv2 = st.number_input()
     
     if st.button("Share Strategy"):
         if not name.strip():
