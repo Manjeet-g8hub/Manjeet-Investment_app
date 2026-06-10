613d67c13827579bee2086b593b57cea00983198
Manjeet-g8hub <ahalawat1986@gmail.com>
2026-06-10 11:28:59 +0000

    added loader

--- Investment-app.py
+++ Investment-app.py
@@ -7,7 +7,7 @@ try:
     
     st.title("💰 Your Personalised AI Investment Assistant")
     
-    st.title("_AI_ is :blue[cool] 📈")
+    st.title("_AI_ is :green[smart] 📈")
     # Input fields for name, inv amt, age and time horizon
     name = st.text_input("* Enter Your name", key="name")
     inv = st.slider("Enter Your investment amount",  min_value=100000, max_value= 100000000, step=100000)
@@ -22,9 +22,12 @@ try:
             prompt = f"Greet {name} and Act like an investment expert, share the investment stratagy based on the amount {inv}, over {time} horizon depending on the age {age}"
         
             # Generate content from Gemini
-            response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            contents= prompt)
-            st.write(response.text)
-except Exception as e:
-    st.error(f"An error occurred: {e}")
+with st.spinner("🤖 AI is generating your personalized investment strategy... Please wait..."):
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+st.success("✅ Strategy generated successfully!")
+st.write(response.text)
